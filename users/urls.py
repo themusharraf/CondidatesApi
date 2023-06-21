519ab076dc8b83a1c749bd913e8935564458a798
themusharraf <meibrohimov@gmail.com>
2023-06-21 11:38:01 +0500

    add Candidate serializers.py TODO urls change  full commit ✅

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from users.views import MyObtainTokenPairView, RegisterView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
+    path('register/', RegisterView.as_view(), name='auth_register'),
     path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', RegisterView.as_view(), name='auth_register'),
 ]
