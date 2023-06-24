e0e116255b923790da8a808dbacdb761abfc71d9
themusharraf <meibrohimov@gmail.com>
2023-06-24 23:35:04 +0500

    serializers.py debug change commit 🧠

--- anketa/views.py
+++ anketa/views.py
@@ -1,19 +1,25 @@
 from django.db.models import F
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from anketa.models import Candidate, Saved
 from anketa.permissions import IsAuthorOrReadOnly
-from anketa.serializers import CandidateDetailModelSerializer
+from anketa.serializers import CandidateDetailModelSerializer, CandidateSerializer
 from rest_framework import generics
 
 
 class CandidateModelViewSet(ModelViewSet):
     queryset = Candidate.objects.all()
     permission_classes = IsAuthorOrReadOnly,
-    serializer_class = CandidateDetailModelSerializer
+    serializer_class = CandidateSerializer
 
-    def get_queryset(self):
-        query = super().get_queryset()
-        query.update(view_count=F('view_count') + 1)
-        return query
-
-
+    def retrieve(self, request, *args, **kwargs):
+        self.get_queryset()
+        instance = self.get_object()
+        instance.view_count += 1
+        instance.save()
+        serializer = CandidateDetailModelSerializer(instance)
+        return Response(serializer.data)
+    # def get_queryset(self):
+    #     query = super().get_queryset()
+    #     query.update(view_count=F('view_count') + 1)
+    #     return query
